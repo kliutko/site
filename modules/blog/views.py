2e99752be6704ba8cd31a98f4eeaff693397317d
kliutko <kliutko.fr@yandex.ru>
2024-08-02 23:34:35 +0300

    create view articles based on class, create urls in models blog

--- modules/blog/views.py
+++ modules/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Article
 # Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/articles_list.html'
+    context_object_name = 'articles'
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
