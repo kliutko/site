bab26e927ebf37dd3c306d859ff5c8587a1470f5
kliutko <kliutko.fr@yandex.ru>
2024-09-14 15:52:17 +0300

    test

--- base/urls.py
+++ base/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.staticfiles.views import serve
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.sitemaps.views import sitemap
@@ -46,8 +47,11 @@ urlpatterns = [
     path('system/', include('modules.system.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
+urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+                re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 if settings.DEBUG:
     urlpatterns = [path('__debug__/', include('debug_toolbar.urls'))] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
